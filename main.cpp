449975c1220fcf1997983a1ee667c0e4e87d5e3d
PaskalZvezda <65816713+PaskalZvezda@users.noreply.github.com>
2026-03-14 05:17:43 +0700

    end.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML");
-    sf::RectangleShape player(sf::Vector2f(30.0f, 30.0f));
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML");   //Инициализация окна
+    sf::RectangleShape player(sf::Vector2f(30.0f, 30.0f));      //Игрок
     player.setFillColor(sf::Color::Blue);
     player.setPosition(375.0f, 275.0f);
-    float speed = 1000.0f; 
+    float speed = 1000.0f;                                      //Скорость
     sf::Clock clock;
     while (window.isOpen()) {
         sf::Event event;
@@ -29,10 +29,9 @@ int main() {
 
         float deltaTime = clock.restart().asSeconds();
         player.move(movement * deltaTime);
-        window.clear(sf::Color::Black); 
+        window.clear(sf::Color::White); 
         window.draw(player);           
         window.display();              
     }
-
     return 0;
 }
